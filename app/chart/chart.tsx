05b2c13382bc10013ab6ffe42b69a35a71bdba73
Aamina <a6anjum@uwaterloo.ca>
2024-07-22 02:23:13 -0400

    lables+charts

--- app/chart/chart.tsx
+++ app/chart/chart.tsx
@@ -24,12 +24,10 @@ export default function Example() {
     }, []);
 
     const data01 = [
-        { name: 'Group A', value: 400 },
-        { name: 'Group B', value: 300 },
-        { name: 'Group C', value: 300 },
-        { name: 'Group D', value: 200 },
-        { name: 'Group E', value: 278 },
-        { name: 'Group F', value: 189 },
+        { name: 'Safety', value: 400 },
+        { name: 'Quality', value: 300 },
+        { name: 'Throughput', value: 300 },
+        { name: 'PIP', value: 200 },
     ];
 
     const data02 = [
@@ -40,6 +38,18 @@ export default function Example() {
         { name: 'Group E', value: 3908 },
         { name: 'Group F', value: 4800 },
     ];
+
+
+// Define the type for the custom label function parameters
+    type CustomLabelProps = {
+        name: string;
+    };
+
+// Custom label function with typed parameters
+    const renderCustomLabel = ({ name }: CustomLabelProps) => {
+        return name;
+    };
+
 
 const CustomTooltip = ({
     active,
@@ -80,35 +90,28 @@ const CustomTooltip = ({
           <Tooltip content={<CustomTooltip />}cursor={{ strokeDasharray: '3 3' }} />
           <Scatter name="A school" data={data} fill="#8884d8" />
         </ScatterChart>
-          <PieChart width={400} height={400}>
-              <Pie
-                  dataKey="value"
-                  isAnimationActive={false}
-                  data={data01}
-                  cx="50%"
-                  cy="50%"
-                  outerRadius={80}
-                  fill="#8884d8"
-                  label
-              />
-              <Pie dataKey="value" data={data02} cx={500} cy={200} innerRadius={40} outerRadius={80} fill="#82ca9d" />
-              <Tooltip />
-          </PieChart>
-          <PieChart width={400} height={400}>
-              <Pie
-                  dataKey="value"
-                  isAnimationActive={false}
-                  data={data01}
-                  cx="50%"
-                  cy="50%"
-                  outerRadius={80}
-                  fill="#8884d8"
-                  label
-              />
-              <Pie dataKey="value" data={data02} cx={500} cy={200} innerRadius={40} outerRadius={80} fill="#82ca9d" />
-              <Tooltip />
-          </PieChart>
+
+
       </ResponsiveContainer>
+            <ResponsiveContainer width={800} height={400}>
+
+
+                <PieChart width={400} height={400}>
+                    <Pie
+                        dataKey="value"
+                        isAnimationActive={true}
+                        data={data01}
+                        cx="30%"
+                        cy="50%"
+                        outerRadius={80}
+                        fill="#8884d8"
+                        label ={renderCustomLabel}
+                    />
+                    <Pie dataKey="value" data={data02} cx={650} cy={200} innerRadius={40} outerRadius={80} fill="#82ca9d" />
+                    <Tooltip />
+                </PieChart>
+            </ResponsiveContainer>
+
 
 
         </div>
