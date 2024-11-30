23229a88c0bb671badeb60f3d69c8ae137118f69
Aamina <a6anjum@uwaterloo.ca>
2024-11-30 13:40:49 -0500

    env file added

--- app/firebase/config.js
+++ app/firebase/config.js
@@ -7,14 +7,15 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDiR862oLiSccRg1d9_C-GL6smhUQ-tPJk",
-  authDomain: "capstone-pfc.firebaseapp.com",
-  projectId: "capstone-pfc",
-  storageBucket: "capstone-pfc.appspot.com",
-  messagingSenderId: "1066520940902",
-  appId: "1:1066520940902:web:66ef9a33b37f153964b8a7",
-  measurementId: "G-PVJ8RE3BJC"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
