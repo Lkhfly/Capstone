c4f93003d877d089162358aa55b08b7199b33f44
Aamina <a6anjum@uwaterloo.ca>
2025-02-10 17:23:01 -0500

    bug fix +image

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,27 @@
 "use client";
 
 import NavBar from '@/components/ui/navbar';
+import Image from 'next/image';
+import severity_image from "../components/ui/TheWellhall.png"
 
 const Home = () => {
     return (
-        <div className="min-h-screen flex flex-col">
+        <div className="relative min-h-screen flex flex-col">
+            {/* Background Image */}
+            <Image 
+                src={severity_image}
+                alt="Background"
+                layout="fill"
+                objectFit="cover"
+                className="-z-10"
+            />
+
+            {/* Navbar */}
             <NavBar />
+
             <main className="flex flex-col flex-grow justify-center items-center container mx-auto px-4">
-                <h1 className="font-bold text-2xl">GM PFC Management Tool</h1>
-                <h1 className="mt-5">Welcome to the tool, we hope you have a great experience</h1>
+               { /*<h1 className="font-bold text-2xl">GM PFC Management Tool</h1>*/}
+                {/*<h1 className="mt-5">Welcome to the tool, we hope you have a great experience</h1>*/}
             </main>
         </div>
     );
