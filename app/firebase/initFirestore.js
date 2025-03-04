11e68020ab1f7c1e1a5edbcec6efcc03b661037e
Khoi Le <khoile.vacl@gmail.com>
2025-03-04 18:12:05 -0500

    Stuff

---
+++ app/firebase/initFirestore.js
@@ -0,0 +1,11 @@
+import admin from "firebase-admin";
+
+if (!admin.apps.length) {
+  const serviceAccount = require("./serviceAccountKey.json");
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+}
+
+const db = admin.firestore();
+export default db;
---
+++ pages/api/firestore-data.js
@@ -0,0 +1,19 @@
+import db from "../../app/firebase/initFirestore"
+
+export default async function handler(req, res) {
+  try {
+    // Fetch data from Firestore
+    const collectionRef = db.collection("pfc"); // Replace with your collection name
+    const snapshot = await collectionRef.get();
+
+    const data = [];
+    snapshot.forEach((doc) => {
+      data.push({ id: doc.id, ...doc.data() });
+    });
+
+    res.status(200).json(data);
+  } catch (error) {
+    console.error("Error fetching data from Firestore:", error);
+    res.status(500).json({ error: "Failed to fetch data from Firestore" });
+  }
+}
