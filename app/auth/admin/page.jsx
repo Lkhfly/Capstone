02a30cb9ebed77945d587d25cd69bea794fc0fa0
Aamina <a6anjum@uwaterloo.ca>
2025-03-10 12:47:25 -0400

    checks

---
+++ app/auth/admin/page.jsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
+const Admin = () => {
+    const [newUser, setNewUser] = useState('');
+    const [newUserConfirmed, setNewUserConfirmed] = useState('');
+
+    return(
+        <div>
+          hi
+        </div>
+    );
+};
+
+export default Admin;
